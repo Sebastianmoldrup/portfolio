86536313efe622e09a506c079bebca229ab7ea17
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2024-01-30 22:01:22 +0100

    updated styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,9 +3,18 @@ import Image from 'next/image';
 
 export default function Contact() {
   return (
-    <div className='flex flex-col justify-center items-center h-full bg-[#101115] text-white p-4 md:pt-6 md:px-8'>
-      <div className='flex flex-col md:flex-row'>
-        <div className='bg-white text-black p-4 space-y-8 shadow-xl md:rounded-l-md rounded-t-md md:rounded-tr-none md:w-[300px] text-center'>
+    <div className='flex justify-center items-center'>
+      <div className='absolute inset-0 bg-gray-300 -z-10'>
+        <iframe
+          width='100%'
+          height='100%'
+          title='map'
+          src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26523.330815202866!2d5.3128765949302155!3d60.389758808408274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sno!2sno!4v1706648260996!5m2!1sno!2sno" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+          style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}
+        ></iframe>
+      </div>
+      <div className='container px-5 py-24 mx-auto flex w-full h-full justify-center items-center mt-20'>
+        <div className='bg-white text-black p-6 space-y-8 shadow-xl md:rounded-l-md rounded-t-md md:rounded-tr-none md:w-[300px] text-center'>
           <h2 className='text-xl font-semibold'>Kontakt meg</h2>
           <div className='space-y-2'>
             <div className='flex space-x-2'>
@@ -22,13 +31,6 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <Image
-          src='/bergenview.webp'
-          width={300}
-          height={300}
-          alt='Picture of the author'
-          className='md:rounded-r-md rounded-b-md md:rounded-bl-none'
-        />
       </div>
     </div>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,35 @@
 export default function Projects() {
   return (
     <div className='flex flex-col justify-center items-center h-full bg-[#101115] text-white p-4 md:pt-6 md:px-8 text-3xl font-bold'>
-      [Prosjekter kommer snart]
+      <div className='p-4 lg:w-1/3'>
+        <div className='h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative'>
+          <h2 className='tracking-widest text-xs title-font font-medium text-gray-400 mb-1'>
+            CATEGORY
+          </h2>
+          <h1 className='title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3'>
+            Raclette Blueberry Nextious Level
+          </h1>
+          <p className='leading-relaxed mb-3'>
+            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+            microdosing tousled waistcoat.
+          </p>
+          <a className='text-indigo-500 inline-flex items-center'>
+            Learn More
+            <svg
+              className='w-4 h-4 ml-2'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              stroke-width='2'
+              fill='none'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            >
+              <path d='M5 12h14'></path>
+              <path d='M12 5l7 7-7 7'></path>
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
