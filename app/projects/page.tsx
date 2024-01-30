1ef3eaf61778219d10fb53897a009aa560bdc7e7
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2024-01-30 22:50:50 +0100

    added project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,18 @@
-import { Button } from '@/components/ui/button';
+import { GlobeIcon } from '@/components/Icons';
 import Image from 'next/image';
-import Link from 'next/link';
 export default function Projects() {
   return (
-    <div className='flex flex-col justify-center items-center h-full bg-[#101115] text-white p-4 md:pt-6 md:px-8 font-bold text-3xl'>
-      {/* <div className='grid rounded-md place-items-center'>
+    <div className='flex flex-col justify-center items-center h-full bg-[#101115] text-white p-4 space-y-6 md:pt-6 md:px-8'>
+      <h2 className='font-bold text-3xl'>[Flere prosjekter kommer snart]</h2>
+      <div className='grid rounded-md place-items-center'>
+        <a
+          href='https://effektivmarkedsforing.no/'
+          target='_blank'
+          className='row-start-1 col-start-1 flex items-center px-2 gap-x-2 text-center bg-[#0f172aea] py-2 rounded-md hover:text-[#0f172aea] hover:bg-white hover:scale-110 z-10 hover:cursor-pointer'
+        >
+          <GlobeIcon />
+          <div className='h-[20px]'>Effektivmarkedsforing.no</div>
+        </a>
         <Image
           src='/effektivmarkedsforing.webp'
           width={400}
@@ -12,11 +20,7 @@ export default function Projects() {
           alt='effektiv markefsførings frontside'
           className='rounded-md opacity-65 hover:opacity-75 row-start-1 col-start-1'
         />
-        <a className='row-start-1 col-start-1 w-[200px] text-center bg-[#0f172aea] py-2 rounded-md hover:text-[#0f172aea] hover:bg-white hover:scale-110'>
-          Effektivmarkedsforing.no
-        </a>
-      </div> */}
-      [Prosjekter kommer snart]
+      </div>
     </div>
   );
 }
