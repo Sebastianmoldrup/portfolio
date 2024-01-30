89c4c9d1449c0d6b2919987a240f9187a3523abb
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2024-01-30 22:12:50 +0100

    minor update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,21 @@
-import { Image } from 'next/image';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 export default function Projects() {
   return (
-    <div className='flex flex-col justify-center items-center h-full bg-[#101115] text-white p-4 md:pt-6 md:px-8 text-3xl font-bold'>
+    <div className='flex flex-col justify-center items-center h-full bg-[#101115] text-white p-4 md:pt-6 md:px-8 font-bold text-3xl'>
+      {/* <div className='grid rounded-md place-items-center'>
+        <Image
+          src='/effektivmarkedsforing.webp'
+          width={400}
+          height={400}
+          alt='effektiv markefsførings frontside'
+          className='rounded-md opacity-65 hover:opacity-75 row-start-1 col-start-1'
+        />
+        <a className='row-start-1 col-start-1 w-[200px] text-center bg-[#0f172aea] py-2 rounded-md hover:text-[#0f172aea] hover:bg-white hover:scale-110'>
+          Effektivmarkedsforing.no
+        </a>
+      </div> */}
       [Prosjekter kommer snart]
     </div>
   );
